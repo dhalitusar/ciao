883946692e87d7974d61ef5e25ab28d6ebea5e16
Tusar <dhalitusar94@gmail.com>
2019-03-01 15:53:24 +0100

    hbjjh
    fff

---
+++ exit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i;
+    for (i=0; i<10; i++){
+        printf("%d\n",i);
+        exit(1);
+    }
+    printf ("Scrittura\n");
+    return 0;
+}
+
+
